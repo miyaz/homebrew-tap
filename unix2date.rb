0edae5fa3ca8b73fcca51716685a97193937a0bf
miyaz <sinji_mg@yahoo.co.jp>
2024-07-16 18:11:38 +0900

    update from 0.0.6 to 0.1.0

--- unix2date.rb
+++ unix2date.rb
@@ -1,27 +1,27 @@
 class Unix2date < Formula
   desc 'unix2date is a command that converts unixtime included in STDIN to datetime and output'
-  version '0.0.6'
+  version '0.1.0'
   homepage 'https://github.com/miyaz/unix2date'
 
   on_macos do
     if Hardware::CPU.arm?
-      url 'https://github.com/miyaz/unix2date/releases/download/v0.0.6/unix2date_0.0.6_darwin_arm64.tar.gz'
-      sha256 '6bf71ab9651e5fbc00755d1c4b1a34b437c4a596ad6c9f66deacc0cf1da7fd48'
+      url 'https://github.com/miyaz/unix2date/releases/download/v0.1.0/unix2date_0.1.0_darwin_arm64.tar.gz'
+      sha256 'db0116e79016e1655cea6082cfa2b33e1245de9e2196e40be2c82e329b216c48'
     end
     if Hardware::CPU.intel?
-      url 'https://github.com/miyaz/unix2date/releases/download/v0.0.6/unix2date_0.0.6_darwin_amd64.tar.gz'
-      sha256 '6e0e5353a392397d9885191b91189e222ee7ff2410a9e13e72e3ec738ee1c6b4'
+      url 'https://github.com/miyaz/unix2date/releases/download/v0.1.0/unix2date_0.1.0_darwin_amd64.tar.gz'
+      sha256 'ec7febc91b79666411988f2f07ed541ac50ff3196062b6a1cbfe2d48f9ea1ee8'
     end
   end
 
   on_linux do
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url 'https://github.com/miyaz/unix2date/releases/download/v0.0.6/unix2date_0.0.6_linux_arm64.tar.gz'
-      sha256 'e54e99ea72046f9cc40d1c97a08574ef53082a6a35b4ea3941f46a1ed65cba1a'
+      url 'https://github.com/miyaz/unix2date/releases/download/v0.1.0/unix2date_0.1.0_linux_arm64.tar.gz'
+      sha256 '78330e961f49d06c52f3114386675bcc3cc6f917b6cf1c0490c8e7372991363c'
     end
     if Hardware::CPU.intel?
-      url 'https://github.com/miyaz/unix2date/releases/download/v0.0.6/unix2date_0.0.6_linux_amd64.tar.gz'
-      sha256 'b1150973b8ba5598b58d8d2a73c5a66620d23fdbc2a6a1bdc48ef238abf41a30'
+      url 'https://github.com/miyaz/unix2date/releases/download/v0.1.0/unix2date_0.1.0_linux_amd64.tar.gz'
+      sha256 'bea61bf3a936b7a8a4ccdccc7a92eeadf0e5952d276b5096342c29219f01001e'
     end
   end
 
