cc12d69c530b26ccfb293e37046e02b04ab025a5
miyaz <sinji_mg@yahoo.co.jp>
2024-08-07 22:26:56 +0900

    update from 0.1.2 to 0.2.0

--- unix2date.rb
+++ unix2date.rb
@@ -1,27 +1,27 @@
 class Unix2date < Formula
   desc 'unix2date is a command that converts unixtime included in STDIN to datetime and output'
-  version '0.1.2'
+  version '0.2.0'
   homepage 'https://github.com/miyaz/unix2date'
 
   on_macos do
     if Hardware::CPU.arm?
-      url 'https://github.com/miyaz/unix2date/releases/download/v0.1.2/unix2date_0.1.2_darwin_arm64.tar.gz'
-      sha256 '753ce375e20f5b2b873e0bc3e453aada7d10b8293e79a2502053b7b9b622a044'
+      url 'https://github.com/miyaz/unix2date/releases/download/v0.2.0/unix2date_0.2.0_darwin_arm64.tar.gz'
+      sha256 'e8928e1c7aa401be05b22b7e2452a883c776c372f8c5c9123008fc745fe2922d'
     end
     if Hardware::CPU.intel?
-      url 'https://github.com/miyaz/unix2date/releases/download/v0.1.2/unix2date_0.1.2_darwin_amd64.tar.gz'
-      sha256 '1279666384ec17525d8610fd079fb4b9a21dfb3c6609b2e4473b6392acda0a89'
+      url 'https://github.com/miyaz/unix2date/releases/download/v0.2.0/unix2date_0.2.0_darwin_amd64.tar.gz'
+      sha256 'eedcdf2908f918c4d84839b99280421e16af0990065b847a366dff3272b18d9f'
     end
   end
 
   on_linux do
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url 'https://github.com/miyaz/unix2date/releases/download/v0.1.2/unix2date_0.1.2_linux_arm64.tar.gz'
-      sha256 '76e07c92a8bf722cbf80297ec80648b349fb32f1ba36348acac1318e4a397680'
+      url 'https://github.com/miyaz/unix2date/releases/download/v0.2.0/unix2date_0.2.0_linux_arm64.tar.gz'
+      sha256 '9af543f39e02e1e5d670a08cbb92d7f50f26dae23e37afbd874b03b2e8444664'
     end
     if Hardware::CPU.intel?
-      url 'https://github.com/miyaz/unix2date/releases/download/v0.1.2/unix2date_0.1.2_linux_amd64.tar.gz'
-      sha256 'fd48f4233dfe6279e174af040eb5ebc1358562eabd0fcfa9c254d23a76a7fc46'
+      url 'https://github.com/miyaz/unix2date/releases/download/v0.2.0/unix2date_0.2.0_linux_amd64.tar.gz'
+      sha256 '36fab3e0517f477a3b3d7b8cc67965a0f20d36904c752b93d483fe157ad3eb7f'
     end
   end
 
